767fef3e36354bee2c014dc16973a8223e6c80a7
Kleb-0 <shadowhilles@gmail.com>
2025-10-29 19:02:24 -0300

    feat: implement QRCodeForm in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import { useState } from "react";
+import QRCodeForm from "./components/QRCodeForm";
+import "./App.css";
+import "./types";
+import type { FormData } from "./types";
 
 function App() {
+  const [formData, setFormData] = useState<FormData>({
+    ssid: "",
+    pass: "",
+    noPass: false,
+    hidden: false,
+  });
+
   return (
-    <div className='flex justify-center'>
-    <h1>QRCode Generator</h1>
+    <div className="flex items-center flex-col">
+      <h1 className="font-bold text-zinc-50">Gerador de QRCode para WI-FI</h1>
+      <QRCodeForm data={formData} setData={setFormData} />
+      <p>{formData.ssid}</p>
+      <p>{formData.pass}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
