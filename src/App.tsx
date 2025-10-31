45bc0e84401d2c3f47c7429d64880bedc1916d64
Kleb-0 <shadowhilles@gmail.com>
2025-10-30 21:40:14 -0300

    feat: add download button with handleDownload function and refactor: change some styles

--- src/App.tsx
+++ src/App.tsx
@@ -27,13 +27,20 @@ function App() {
     setQrCodeUrl(url);
   }
 
+  function handleDownload() {
+    const link = document.createElement("a");
+    link.href = qrCodeUrl;
+    link.download = `wifi-qrcode-${formData.ssid}`;
+    link.click();
+  }
+
   return (
-    <div className="flex items-center flex-col">
-      <h1 className="font-bold text-zinc-50 mb-10">
+    <div className="flex items-center flex-col px-4 pt-4 pb-8">
+      <h1 className="font-bold text-zinc-50 mb-10 text-center">
         Gerador de QRCode para WI-FI
       </h1>
-      <div className="flex gap-8">
-        <div className="w-96">
+      <div className="flex flex-col lg:flex-row gap-8 items-center">
+        <div className="w-full sm:w-64">
           <QRCodeForm
             data={formData}
             setData={setFormData}
@@ -43,14 +50,22 @@ function App() {
         </div>
 
         {qrCodeUrl && (
-          <div className="flex items-center gap-8">
-            <ArrowBigRight className="w-16 h-16" />
-            <div className="w-96">
-              <img
-                className="rounded-lg w-full h-full"
-                src={qrCodeUrl}
-                alt="QRCode"
-              />
+          <div className="flex flex-col lg:flex-row items-center sm:gap-8">
+            <ArrowBigRight className="hidden lg:block w-16 h-16" />
+            <div className="flex flex-col items-center gap-8">
+              <div className="w-1/2 sm:w-64">
+                <img
+                  className="rounded-lg w-full h-full"
+                  src={qrCodeUrl}
+                  alt="QRCode"
+                />
+              </div>
+              <button
+                className="w-full bg-zinc-50 text-zinc-900 hover:bg-purple-700 hover:text-zinc-50 text-sm sm:text-md"
+                onClick={handleDownload}
+              >
+                Baixar
+              </button>
             </div>
           </div>
         )}
