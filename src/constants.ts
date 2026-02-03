bba6e03dff65ae3382c47ef94771cbb555fbea59
Kleb-0 <shadowhilles@gmail.com>
2026-02-03 18:09:37 -0300

    feat: add qrCodeLayout and validationRules constants

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const qrCodeLayout = {
+  width: 600,
+  margin: 2,
+};
+
+export const validationRules = {
+  ssid: {
+    minLength: 1,
+    maxLength: 32,
+  },
+  pass: {
+    minLength: 8,
+    maxLength: 63,
+  },
+};
